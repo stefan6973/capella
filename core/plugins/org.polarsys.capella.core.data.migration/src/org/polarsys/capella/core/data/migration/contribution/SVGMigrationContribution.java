b41905e6332e7be587c9e4c1b9e39ff1da61ed48
Philippe DUL <philippe.dul@thalesgroup.com>
2021-10-19 13:49:40 +0200

    Draft images

--- core/plugins/org.polarsys.capella.core.data.migration/src/org/polarsys/capella/core/data/migration/contribution/SVGMigrationContribution.java
+++ core/plugins/org.polarsys.capella.core.data.migration/src/org/polarsys/capella/core/data/migration/contribution/SVGMigrationContribution.java
@@ -12,11 +12,9 @@
  *******************************************************************************/
 package org.polarsys.capella.core.data.migration.contribution;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
 import org.eclipse.draw2d.geometry.Dimension;
@@ -24,8 +22,10 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gmf.runtime.notation.Bounds;
 import org.eclipse.gmf.runtime.notation.Node;
 import org.eclipse.sirius.diagram.AbstractDNode;
+import org.eclipse.sirius.diagram.DDiagram;
+import org.eclipse.sirius.diagram.DNode;
+import org.eclipse.sirius.diagram.DSemanticDiagram;
 import org.eclipse.sirius.diagram.WorkspaceImage;
-import org.eclipse.sirius.diagram.business.internal.metamodel.spec.DSemanticDiagramSpec;
 import org.eclipse.sirius.viewpoint.Style;
 import org.osgi.framework.Version;
 import org.polarsys.capella.core.data.migration.context.MigrationContext;
@@ -35,10 +35,9 @@ public class SVGMigrationContribution extends AbstractMigrationContribution {
    * Map containing the workspace paths of the images replaced and what they're replaced with.
    */
   public static final Map<String, String> CHANGED_IMAGES;
-  
-  
+
   public static final Map<String, Map<String, Dimension>> DIFFERENT_IMAGE_SIZE_PER_DIAGRAM;
-  
+
   static {
     Map<String, String> changedImagesTemp = new HashMap();
     changedImagesTemp.put("/org.polarsys.capella.core.ui.resources/icons/full/png/SystemActor.png",
@@ -88,70 +87,71 @@ public class SVGMigrationContribution extends AbstractMigrationContribution {
     changedImagesTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/handlelifeline.png",
         "/org.polarsys.capella.core.sirius.analysis/description/images/handlelifeline.svg");
     CHANGED_IMAGES = Collections.unmodifiableMap(changedImagesTemp);
-//to be deleted after im done
-    
-//    Map<String, Map<String, Dimension>> differentSizePerDiagram = new HashMap<>();
-//    Map<String, Dimension> differentSizeTemp = new HashMap<>();
-//    differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionDuplicateDiagram.svg", new Dimension(56,55));
-//    differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionGatherDiagram.svg", new Dimension(56,56));
-//    differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionRouteDiagram.svg", new Dimension(56,55));
-//    differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionSelectDiagram.svg", new Dimension(56,56));
-//    differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionSplitDiagram.svg", new Dimension(56,56));
-//    differentSizePerDiagram.put("Data Flow Blank", differentSizeTemp);
-//    differentSizeTemp.clear();
-//    
-//    differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionDuplicateDiagram.svg", new Dimension(50,50));
-//    differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionGatherDiagram.svg", new Dimension(50,50));
-//    differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionRouteDiagram.svg", new Dimension(50,50));
-//    differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionSelectDiagram.svg", new Dimension(50,50));
-//    differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionSplitDiagram.svg", new Dimension(50,50));
-//    differentSizePerDiagram.put("Architecture Blank", differentSizeTemp);
-//    differentSizeTemp.clear();
-//    
-//    differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionDuplicateDiagram.svg", new Dimension(50,49));
-//    differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionGatherDiagram.svg", new Dimension(50,50));
-//    differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionRouteDiagram.svg", new Dimension(50,49));
-//    differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionSelectDiagram.svg", new Dimension(50,50));
-//    differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionSplitDiagram.svg", new Dimension(50,50));
-//    differentSizePerDiagram.put("Functional Chain Description", differentSizeTemp);
-//    differentSizeTemp.clear();
-//    
-    
-    
+    // to be deleted after im done
+
+    // Map<String, Map<String, Dimension>> differentSizePerDiagram = new HashMap<>();
+    // Map<String, Dimension> differentSizeTemp = new HashMap<>();
+    // differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionDuplicateDiagram.svg",
+    // new Dimension(56,55));
+    // differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionGatherDiagram.svg",
+    // new Dimension(56,56));
+    // differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionRouteDiagram.svg",
+    // new Dimension(56,55));
+    // differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionSelectDiagram.svg",
+    // new Dimension(56,56));
+    // differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionSplitDiagram.svg",
+    // new Dimension(56,56));
+    // differentSizePerDiagram.put("Data Flow Blank", differentSizeTemp);
+    // differentSizeTemp.clear();
+    //
+    // differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionDuplicateDiagram.svg",
+    // new Dimension(50,50));
+    // differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionGatherDiagram.svg",
+    // new Dimension(50,50));
+    // differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionRouteDiagram.svg",
+    // new Dimension(50,50));
+    // differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionSelectDiagram.svg",
+    // new Dimension(50,50));
+    // differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionSplitDiagram.svg",
+    // new Dimension(50,50));
+    // differentSizePerDiagram.put("Architecture Blank", differentSizeTemp);
+    // differentSizeTemp.clear();
+    //
+    // differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionDuplicateDiagram.svg",
+    // new Dimension(50,49));
+    // differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionGatherDiagram.svg",
+    // new Dimension(50,50));
+    // differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionRouteDiagram.svg",
+    // new Dimension(50,49));
+    // differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionSelectDiagram.svg",
+    // new Dimension(50,50));
+    // differentSizeTemp.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionSplitDiagram.svg",
+    // new Dimension(50,50));
+    // differentSizePerDiagram.put("Functional Chain Description", differentSizeTemp);
+    // differentSizeTemp.clear();
+    //
+
     Map<String, Map<String, Dimension>> differentSizePerDiagram = new HashMap<>();
     Map<String, Dimension> differentSizeTemp = new HashMap<>();
-    differentSizeTemp.put("Data Flow Blank", new Dimension(56,55));
-    differentSizeTemp.put("Architecture Blank", new Dimension(50,50));
-    differentSizeTemp.put("Functional Chain Description", new Dimension(50,49));
-    differentSizePerDiagram.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionDuplicateDiagram.svg", differentSizeTemp);
+    differentSizeTemp.put("Data Flow Blank", new Dimension(55, 56));
+    differentSizeTemp.put("Architecture Blank", new Dimension(50, 50));
+    differentSizeTemp.put("Functional Chain Description", new Dimension(50, 50));
+    differentSizePerDiagram.put(
+        "/org.polarsys.capella.core.sirius.analysis/description/images/FunctionDuplicateDiagram.svg",
+        differentSizeTemp);
+    differentSizePerDiagram.put(
+        "/org.polarsys.capella.core.sirius.analysis/description/images/FunctionGatherDiagram.svg", differentSizeTemp);
+    differentSizePerDiagram.put(
+        "/org.polarsys.capella.core.sirius.analysis/description/images/FunctionRouteDiagram.svg", differentSizeTemp);
+    differentSizePerDiagram.put(
+        "/org.polarsys.capella.core.sirius.analysis/description/images/FunctionSelectDiagram.svg", differentSizeTemp);
+    differentSizePerDiagram.put(
+        "/org.polarsys.capella.core.sirius.analysis/description/images/FunctionSplitDiagram.svg", differentSizeTemp);
     differentSizeTemp = new HashMap<>();
 
-    differentSizeTemp.put("Data Flow Blank", new Dimension(56,56));
-    differentSizeTemp.put("Architecture Blank", new Dimension(50,50));
-    differentSizeTemp.put("Functional Chain Description", new Dimension(50,50));
-    differentSizePerDiagram.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionGatherDiagram.svg", differentSizeTemp);
-    differentSizeTemp = new HashMap<>();
-
-    differentSizeTemp.put("Data Flow Blank", new Dimension(56,55));
-    differentSizeTemp.put("Architecture Blank", new Dimension(50,50));
-    differentSizeTemp.put("Functional Chain Description", new Dimension(50,49));
-    differentSizePerDiagram.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionRouteDiagram.svg", differentSizeTemp);
-    differentSizeTemp = new HashMap<>();
-
-    differentSizeTemp.put("Data Flow Blank", new Dimension(56,56));
-    differentSizeTemp.put("Architecture Blank", new Dimension(50,50));
-    differentSizeTemp.put("Functional Chain Description", new Dimension(50,50));
-    differentSizePerDiagram.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionSelectDiagram.svg", differentSizeTemp);
-    differentSizeTemp = new HashMap<>();
-
-    differentSizeTemp.put("Data Flow Blank", new Dimension(56,56));
-    differentSizeTemp.put("Architecture Blank", new Dimension(50,50));
-    differentSizeTemp.put("Functional Chain Description", new Dimension(50,50));
-    differentSizePerDiagram.put("/org.polarsys.capella.core.sirius.analysis/description/images/FunctionSplitDiagram.svg", differentSizeTemp);
-    
     DIFFERENT_IMAGE_SIZE_PER_DIAGRAM = Collections.unmodifiableMap(differentSizePerDiagram);
   }
-  
+
   /*
    * If the model has a valid version, go through every diagram element, check if its one of the changed ones and if so
    * replace its image.
@@ -159,57 +159,62 @@ public class SVGMigrationContribution extends AbstractMigrationContribution {
   @Override
   public void unaryMigrationExecute(EObject currentElement, MigrationContext context) {
     super.unaryMigrationExecute(currentElement, context);
+
     if (isValidModelVersion(context) && currentElement instanceof AbstractDNode) {
-      Style elementStyle = ((AbstractDNode) currentElement).getStyle();
+      AbstractDNode node = ((AbstractDNode) currentElement);
+      Style elementStyle = node.getStyle();
+
       if (elementStyle instanceof WorkspaceImage) {
         WorkspaceImage image = ((WorkspaceImage) elementStyle);
         String newWorkspacePath = CHANGED_IMAGES.get(image.getWorkspacePath());
         if (newWorkspacePath != null) {
           image.setWorkspacePath(newWorkspacePath);
-          changeSizeOf(currentElement, newWorkspacePath);
-          elementStyle.getCustomFeatures().add("workspacePath");
+        }
+        changeSizeOf(node, image.getWorkspacePath());
+      }
+    }
+  }
+
+  private void changeSizeOf(AbstractDNode currentElement, String newWorkspacePath) {
+    if (DIFFERENT_IMAGE_SIZE_PER_DIAGRAM.containsKey(newWorkspacePath)) {
+      DDiagram diagram = currentElement.getParentDiagram();
+      Dimension dimension = getNewDimensionOfElementWithinDiagram(diagram, newWorkspacePath);
+      Node node = getNode(diagram, currentElement);
+      if (node != null && node.getLayoutConstraint() instanceof Bounds) {
+        Bounds bounds = (Bounds) (node.getLayoutConstraint());
+        // If user hasn't changed the size by himself, we force to the previous size
+        if (bounds != null && bounds.getWidth() == -1 && bounds.getHeight() == -1) {
+          bounds.setWidth(dimension.width());
+          bounds.setHeight(dimension.height());
+        }
+        // If we don't set '4' in here like defined in Odesign, Sirius will set back the size at the first refresh.
+        if (currentElement instanceof DNode) {
+          ((DNode) currentElement).setWidth(4);
+          ((DNode) currentElement).setHeight(4);
         }
       }
     }
   }
 
-  private void changeSizeOf(EObject currentElement, String newWorkspacePath) {
-    if(DIFFERENT_IMAGE_SIZE_PER_DIAGRAM.containsKey(newWorkspacePath)){
-      DSemanticDiagramSpec diagram = getDiagramOf(currentElement);
-      final Iterator<EObject> it = diagram.eAllContents();
-      Dimension dimension = getNewDimensionOfElementWithinDiagram(diagram, newWorkspacePath);
-      while(it.hasNext()) {
-        final EObject curr = it.next();
-        if(curr instanceof Node) {
-          if(((Node)curr).getElement() == currentElement) {
-            Bounds bounds = (Bounds)(((Node)curr).getLayoutConstraint());
-            bounds.setWidth(dimension.width());
-            bounds.setHeight(dimension.height());
-            return;
-          }
+  private Node getNode(DDiagram diagram, AbstractDNode currentElement) {
+    final Iterator<EObject> it = diagram.eAllContents();
+    while (it.hasNext()) {
+      final EObject curr = it.next();
+      if (curr instanceof Node) {
+        if (((Node) curr).getElement() == currentElement) {
+          return (Node) curr;
         }
       }
-      
     }
+    return null;
   }
-  
-  private Dimension getNewDimensionOfElementWithinDiagram(DSemanticDiagramSpec diagram, String newWorkspacePath) {
+
+  private Dimension getNewDimensionOfElementWithinDiagram(DDiagram diagram, String newWorkspacePath) {
     String diagramType = diagram.getDescription().getName();
-    return DIFFERENT_IMAGE_SIZE_PER_DIAGRAM.get(newWorkspacePath).
-        entrySet().stream().filter(entry -> diagramType.contains(entry.getKey()))
-        .map(Map.Entry::getValue).findFirst().orElse(null);
+    return DIFFERENT_IMAGE_SIZE_PER_DIAGRAM.get(newWorkspacePath).entrySet().stream()
+        .filter(entry -> diagramType.contains(entry.getKey())).map(Map.Entry::getValue).findFirst().orElse(null);
   }
-  
-  private DSemanticDiagramSpec getDiagramOf(EObject currentElement) {
-    if(currentElement instanceof DSemanticDiagramSpec)
-      return (DSemanticDiagramSpec) currentElement;
-    else if (currentElement == null)
-      return null;
-    else return getDiagramOf(currentElement.eContainer());
-  }
-  
-  
-  
+
   private boolean isValidModelVersion(MigrationContext context) {
     Version version = context.getCurrentVersion();
     return version.getMajor() < 5 || (version.getMajor() == 5 && version.getMinor() <= 2);
